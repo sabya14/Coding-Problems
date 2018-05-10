c89b55aef75d742082682c89cee5f5cba30eb55f
neel <sabyasachi.nandy@vphrase.com>
2018-05-11 00:23:25 +0530

    Submartix Updation - Solved Full , Fastest in Python3 HackerEarth

--- Data Structures/Multi-Dimensional Arrays/Submatrix Updates.py
+++ Data Structures/Multi-Dimensional Arrays/Submatrix Updates.py
@@ -4,27 +4,26 @@ practice-problems/algorithm/submatrix-updates-1/
 """
 
 no_of_rows, no_of_columns, no_of_operations = map(int, input().split())
-array = []
+alist = []
+blist = [(no_of_columns+1)*[0] for i in range(no_of_rows+1)]
+
 for i in range(no_of_rows):
-	temp = list(map(int, input().split()))
-	array.append(temp)
+	alist.append(list(map(int, input().split())))
+	blist.append([0] * no_of_rows)
 
 for i in range(no_of_operations):
-	row, column, size, to_add = map(int, input().split())
-	if row + size > no_of_rows + 1:
-		row_limit = no_of_rows
-	else:
-		row_limit = row + size
+	r, c, s, d = [int(i) for i in input().split()]
+	for i in range(r - 1, r - 1 + s):
+		blist[i][c - 1] += d
+		blist[i][c - 1 + s] -= d
 
-	if column + size > no_of_columns + 1:
-		col_limit = no_of_rows
-	else:
-		col_limit = column + size
-	for row in range(row - 1, row_limit - 1):
-		for col in range(column - 1, col_limit - 1):
-			array[row][col] = array[row][col] + to_add
+for row in range(no_of_rows + 1):
+	for col in range(1, no_of_columns + 1):
+		blist[row][col] = blist[row][col] + blist[row][col - 1]
 
 for row in range(no_of_rows):
 	for col in range(no_of_columns):
-		print(array[row][col], end =" ")
-	print("")
+		alist[row][col] = alist[row][col] + blist[row][col]
+
+for row in alist:
+	print(*row)
